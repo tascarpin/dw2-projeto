fa2548f4a77c72e0599bfbf58d4c0b515cf92eb2
superior <superior@LAB2-PC-PROFESSOR>
2019-10-04 20:03:54 -0300

    Criação das classes produto e pedido com seus métodos e atributos.

--- src/app/model/entity/Pedido.php
+++ src/app/model/entity/Pedido.php
@@ -3,5 +3,101 @@ namespace src\app\model\entity;
 
 class Pedido
 {
+    private $idPedido;
+    private $codigo;
+    private $operadorCaixa;
+    private $desconto;
+    private $total;
+    private $pagamento;
+    /**
+     * @return mixed
+     */
+    public function getIdPedido()
+    {
+        return $this->idPedido;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCodigo()
+    {
+        return $this->codigo;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getOperadorCaixa()
+    {
+        return $this->operadorCaixa;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDesconto()
+    {
+        return $this->desconto;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTotal()
+    {
+        return $this->total;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPagamento()
+    {
+        return $this->pagamento;
+    }
+
+    /**
+     * @param mixed $codigo
+     */
+    public function setCodigo($codigo)
+    {
+        $this->codigo = $codigo;
+    }
+
+    /**
+     * @param mixed $operadorCaixa
+     */
+    public function setOperadorCaixa($operadorCaixa)
+    {
+        $this->operadorCaixa = $operadorCaixa;
+    }
+
+    /**
+     * @param mixed $desconto
+     */
+    public function setDesconto($desconto)
+    {
+        $this->desconto = $desconto;
+    }
+
+    /**
+     * @param mixed $total
+     */
+    public function setTotal($total)
+    {
+        $this->total = $total;
+    }
+
+    /**
+     * @param mixed $pagamento
+     */
+    public function setPagamento($pagamento)
+    {
+        $this->pagamento = $pagamento;
+    }
+
+    
+    
 }
 
--- src/app/model/entity/Produto.php
+++ src/app/model/entity/Produto.php
@@ -3,6 +3,134 @@ namespace src\app\model\entity;
 
 class Produto
 {
+    private $idProduto;
+    private $nome;
+    private $descricao;
+    private $precoCusto;
+    private $precoVenda;
+    private $fornecedor;
+    private $tipo;
+    private $status;
+    /**
+     * @return mixed
+     */
+    public function getIdProduto()
+    {
+        return $this->idProduto;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDescricao()
+    {
+        return $this->descricao;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPrecoCusto()
+    {
+        return $this->precoCusto;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPrecoVenda()
+    {
+        return $this->precoVenda;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFornecedor()
+    {
+        return $this->fornecedor;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTipo()
+    {
+        return $this->tipo;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * @param mixed $nome
+     */
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+    }
+
+    /**
+     * @param mixed $descricao
+     */
+    public function setDescricao($descricao)
+    {
+        $this->descricao = $descricao;
+    }
+
+    /**
+     * @param mixed $precoCusto
+     */
+    public function setPrecoCusto($precoCusto)
+    {
+        $this->precoCusto = $precoCusto;
+    }
+
+    /**
+     * @param mixed $precoVenda
+     */
+    public function setPrecoVenda($precoVenda)
+    {
+        $this->precoVenda = $precoVenda;
+    }
+
+    /**
+     * @param mixed $fornecedor
+     */
+    public function setFornecedor($fornecedor)
+    {
+        $this->fornecedor = $fornecedor;
+    }
+
+    /**
+     * @param mixed $tipo
+     */
+    public function setTipo($tipo)
+    {
+        $this->tipo = $tipo;
+    }
+
+    /**
+     * @param mixed $status
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+    }
+
     
 }
 
